eaa062c3a23991ee4060e50aa130fbe6e8aa3017
Fr-noname <frnikita.91@gmail.com>
2025-03-30 13:32:17 +0300

    Всё соответствует формату

--- data/pages.py
+++ data/pages.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from data import db_session
-from data.news import News
+from data.books import Book
 from data.users import User
 from forms.news import NewsForm
 from forms.user import RegisterForm, LoginForm
-
-
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -16,7 +14,7 @@ app.config['SECRET_KEY'] = 'qwertyuiopasdfghjkl_ghhasdfjksdfghsdaf_ebanoeafaffd_
 
 
 def main():
-    db_session.global_init("db/blogs.db")
+    db_session.global_init("db/db.db")
     app.run(port=8080, host='127.0.0.1')
 
 
@@ -25,11 +23,13 @@ def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
+
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
+    book = db_sess.query(Book)
+    return render_template("index.html", news=book, title='Главная страница')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
@@ -53,6 +53,7 @@ def reqister():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -65,10 +66,12 @@ def login():
         return render_template("login.html", message="Неправильный логин или пароль", form=form)
     return render_template("login.html", title="Авторизация", form=form)
 
+
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect("/")
+
 
 # Пробуем запустить.
 
@@ -79,17 +82,17 @@ def add_news():
     form = NewsForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        news = News()
-        news.title = form.title.data
-        news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
+        book = Book()
+        book.title = form.title.data
+        book.content = form.content.data
+        current_user.book.append(book)
         #  Изменили текущего пользователя с помощью метода merge:
         db_sess.merge(current_user)
         db_sess.commit()
         return redirect('/')
     return render_template('news.html', title='Добавление новости',
                            form=form)
+
 
 # Пробуем запустить
 
@@ -102,8 +105,8 @@ def edit_news(id):
     if request.method == "GET":
         # ищем ее в базе по id, причем автор новости должен совпадать с текущим пользователем.
         db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id,
-                                          News.user == current_user
+        news = db_sess.query(Book).filter(Book.id == id,
+                                          Book.user == current_user
                                           ).first()
         if news:
             # Если что-то нашли, предзаполняем форму:
@@ -116,8 +119,8 @@ def edit_news(id):
     # Такую же проверку на всякий случай делаем перед изменением новости.
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id,
-                                          News.user == current_user
+        news = db_sess.query(Book).filter(Book.id == id,
+                                          Book.user == current_user
                                           ).first()
         if news:
             news.title = form.title.data
@@ -132,6 +135,7 @@ def edit_news(id):
                            form=form
                            )
 
+
 # 3-3 Добавляем в шаблон index.html кнопки: "Изменить" и "Удалить"
 
 # Пробуем запустить
@@ -141,8 +145,8 @@ def edit_news(id):
 @login_required
 def news_delete(id):
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.id == id,
-                                      News.user == current_user
+    news = db_sess.query(Book).filter(Book.id == id,
+                                      Book.user == current_user
                                       ).first()
     if news:
         db_sess.delete(news)
