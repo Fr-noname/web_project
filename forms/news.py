6718db3b4dc640455b04b7775368059c0657c64d
Your Name <you@example.com>
2025-03-30 15:50:43 +0300

    чуть чуть подправил форму

--- forms/news.py
+++ forms/news.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class NewsForm(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired()])
-    content = TextAreaField("Содержание")
+    title = StringField('Заголовок',
+                        validators=[DataRequired(), Length(min=1, max=100, message='Заголовок должен быть от 1 до 100 символов.')])
+    content = TextAreaField("Содержание", validators=[DataRequired(message='Содержание является обязательным.')])
     is_private = BooleanField("Личное")
     submit = SubmitField('Применить')
